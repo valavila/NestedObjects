b349ad856a541ec42b8715087a388bac6a990789
Valeria Avila <valeria.avila17@gmail.com>
2020-07-27 03:40:18 -0700

    Added student class

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an indivdual student
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// Legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Legal last name
+        /// </summary>
+        public string LastName { get; set; }
+    }
+}
