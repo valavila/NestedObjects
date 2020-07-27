be2e4fc8d2a201b62549424834028c33218a9340
Valeria Avila <valeria.avila17@gmail.com>
2020-07-27 03:44:20 -0700

    Added instrctor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// Legal full name. First and last
+        /// ex. Jane Doe
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email address
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
